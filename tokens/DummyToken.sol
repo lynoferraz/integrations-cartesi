d49f65e6ee4617846c600f438bd184ac2f76b39c
lynoferraz <70952897+lynoferraz@users.noreply.github.com>
2022-12-02 14:49:23 -0300

    Update DummyToken.sol

--- tokens/DummyToken.sol
+++ tokens/DummyToken.sol
@@ -5,6 +5,6 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract DummyToken is ERC20 {
     constructor() ERC20("DummyToken", "DT") {
-        _mint(msg.sender, 1000000000);
+        _mint(msg.sender, 1000000000000000000000);
     }
 }
